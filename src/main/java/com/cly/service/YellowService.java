de19a475f00d0658f95187d0c531b4d6b61ceae4
alert <5718211151@qq.com>
2018-05-24 08:51:26 +0800

    Revert "跨域"
    This reverts commit 72c82cf7af304e866aea7b07d358ba3933d280ed.

---
+++ src/main/java/com/cly/service/YellowService.java
@@ -0,0 +1,14 @@
+package com.cly.service;
+
+import com.cly.model.YellowObject;
+
+import java.util.List;
+
+/**
+ * @author ：Chen lingyun
+ * The best writter in the world
+ **/
+public interface YellowService {
+
+    List<YellowObject>  query();
+}
